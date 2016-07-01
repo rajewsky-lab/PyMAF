b3262dd860d076d603c2410f3675c43fb4ad8a28
Marvin Jens <mjens@mit.edu>
2016-07-01 11:40:29 -0400

    * Added proper commandline interface and starting from .gz files without decompressed intermediates

--- maf_sparse_index.py
+++ maf_sparse_index.py
@@ -5,8 +5,6 @@ import optparse
 import logging
 from byo.io.lz import LZFile
 
-logging.basicConfig(level=logging.DEBUG)
-log = logging.getLogger('maf_sparse_index.py')
 
 def pileup_loci(loci_list):
     """
@@ -57,14 +55,28 @@ def pileup_loci(loci_list):
         loci.pop(0)
         ends.pop(0)
 
-def maf_to_loci(path,ref):
+def maf_to_loci(path,ref, compress=True):
     maf_list = []
     prefix = "s %s." % ref
     i = 0
     prev_i = 0
     total = 0
 
-    for maf_line in LZFile(path, compress_on_open=True):
+    # preparing the MAF file
+    if path.endswith('.gz'):
+        # directly read from GZ compressed input
+        from gzip import GzipFile
+        lz = LZFile(
+            path.replace('.gz',''), 
+            compress_on_open=compress, 
+            alt_src = GzipFile(path, 'rb'),
+            chunksize = options.chunksize * 1024**2
+        )
+    else:
+        # read from uncompressed input
+        lz = LZFile(path, compress_on_open=compress, chunksize = options.chunksize * 1024**2)
+
+    for maf_line in lz:
         if maf_line.startswith(prefix):
             parts = re.split(r'\s+',maf_line)
             start,size,strand,total = parts[2:6]
@@ -78,29 +90,52 @@ def maf_to_loci(path,ref):
         print "reference '%s' never occured in this MAF file. Are you sure you are doing this right?" % ref
     return maf_list,int(total)
 
-fname = sys.argv[1]
-ref = sys.argv[2]
 
-path = os.path.dirname(fname)
-name = os.path.basename(fname)
+if __name__ == "__main__":
+    from optparse import OptionParser
+    usage = """
+    usage: %prog [options] <MAF_file.maf|MAF_file.maf.gz> <reference_species>
+    """
 
-comb_fname = os.path.join(path,name.replace(".maf",".comb_idx"))
-bin_fname = os.path.join(path,name.replace(".maf",".comb_bin"))
+    parser = OptionParser(usage=usage)
+    parser.add_option("","--debug",dest="debug",default=False,action="store_true",help="activate extensive debug output (default=Off)")
+    parser.add_option("-u","--uncompressed", dest="uncompressed", default=False, action="store_true", help='do not automatically compress the MAF with LZ4 (default is compression)')
+    parser.add_option("", "--chunksize", dest="chunksize", type=int, default=10, help="chunk-size in MB (default=10)")
+    parser.add_option("", "--lz-level", dest="lz_level", type=int, default=2, help="LZ4 compression level. Balance speed vs space (default=2)")
+    options,args = parser.parse_args()
 
-log.info("collecting maf-blocks from '%s' for reference '%s'" % (fname,ref))
-maf_list,chrom_size = maf_to_loci(fname,ref)
-log.info("collected %d blocks." % len(maf_list) )
+    # prepare logging system
+    FORMAT = '%(asctime)-20s\t%(levelname)s\t%(name)-25s\t%(message)s'
+    if options.debug:
+        logging.basicConfig(level=logging.DEBUG,format=FORMAT)
+    else:
+        logging.basicConfig(level=logging.INFO,format=FORMAT)
+    logger = logging.getLogger('maf_sparse_index.main()')
 
-log.info("writing combinations to '%s' and lookup to binary sparse file '%s'" % (comb_fname,bin_fname))
-comb = file(comb_fname,"w")
-lkup = np.memmap(bin_fname,dtype=np.uint32,mode="w+",shape=(chrom_size,))
+    fname, ref = args
 
-for j,(combination,start,end) in enumerate(pileup_loci(maf_list)):
-    lkup[start:end] = j+1
+    path = os.path.dirname(fname)
+    name = os.path.basename(fname)
+
+    comb_fname = os.path.join(path,name.replace(".maf",".comb_idx").replace('.gz',''))
+    bin_fname = os.path.join(path,name.replace(".maf",".comb_bin").replace('.gz',''))
+
+    logger.info("collecting maf-blocks from '{0}' for reference '{1}'".format(fname,ref))
+    maf_list,chrom_size = maf_to_loci(fname,ref, compress=not options.uncompressed)
     
-    comb_str = ",".join(["%x" % maf_line for start,end,maf_line in combination]) + '\n'
-    comb.write(comb_str)
-    
-    if j and not j % 100000:
-        perc = 100. * start / float(chrom_size)
-        log.debug("%.2f %% of %s" % (perc,name))
+    logger.info("collected {0} blocks.".format(len(maf_list)) )
+
+    logger.info("writing combinations to '{0}' and lookup to binary sparse file '{1}'".format(comb_fname,bin_fname))
+    comb = file(comb_fname,"w")
+    lkup = np.memmap(bin_fname,dtype=np.uint32,mode="w+",shape=(chrom_size,))
+
+    for j,(combination,start,end) in enumerate(pileup_loci(maf_list)):
+        lkup[start:end] = j+1
+        
+        comb_str = ",".join(["%x" % maf_line for start,end,maf_line in combination]) + '\n'
+        comb.write(comb_str)
+        
+        if j and not j % 100000:
+            perc = 100. * start / float(chrom_size)
+            logger.debug("{0:.2f} % of {1}".format(perc,name))
+            
