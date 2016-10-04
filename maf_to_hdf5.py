6a673e0b1191cc82744b48f4328d2274eca91010
Marvin Jens <mjens@mit.edu>
2016-10-04 19:21:32 -0400

    * connect to flush() changes in maf_hdf5.py

--- maf_to_hdf5.py
+++ maf_to_hdf5.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     parser.add_option("","--debug",dest="debug",default=False,action="store_true",help="activate extensive debug output (default=Off)")
     parser.add_option("","--flush",dest="flush",default=10000,type=int,help="number of blocks to process before flushing to disk (default=10000)")
     parser.add_option("","--threads",dest="threads",default=8,type=int,help="max. number of threads to use for BLOSC compressor (default=8)")
+    parser.add_option("","--complevel",dest="complevel",default=2,type=int,help="BLOSC compression level (0=off, 10=max, default=2)")
     options,args = parser.parse_args()
 
     # prepare logging system
@@ -29,5 +30,5 @@ if __name__ == "__main__":
     if not args:
         parser.error("path to MAF required")
     else:
-        hdf5.create_from_gz(args[0])
+        hdf5.create_from_gz(args[0], regular_flushes=options.flush, blosc_max_threads=options.threads, complevel=options.complevel)
     
